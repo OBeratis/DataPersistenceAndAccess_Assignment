cb586d3fb95bc15b62982cf802972620923f0097
OBeratis <odysseus.beratis@de.experis.com>
2022-02-23 11:37:39 +0100

    FEAT: Add diagram as PDF files

--- SuperheroesDB Scripts/04_relationshipSuperheroPower.sql
+++ SuperheroesDB Scripts/04_relationshipSuperheroPower.sql
@@ -12,18 +12,26 @@
 USE [SuperheroesDb];
 GO
 
---ALTER TABLE [dbo].[Superhero] ADD CONSTRAINT [FK_PowerId]
---    FOREIGN KEY ([PowerId]) REFERENCES [dbo].[Power] ([PowerId]) ON DELETE NO ACTION ON UPDATE NO ACTION;
---GO
---CREATE INDEX [IFK_PowerId] ON [dbo].[Superhero] ([PowerId]);
---GO
+DROP TABLE IF EXISTS [dbo].[Attributes];
 
 CREATE TABLE [dbo].[Attributes]
 (
-    [AttributeId] INT IDENTITY(1,1) NOT NULL,
+	[AttributeId] INT IDENTITY(1,1) NOT NULL,
 	[SuperheroId] INT FOREIGN KEY REFERENCES [dbo].[Superhero](SuperheroId),
 	[PowerId] INT FOREIGN KEY REFERENCES [dbo].[Power](PowerId),
-    CONSTRAINT [PK_Attributes] PRIMARY KEY CLUSTERED ([AttributeId])
+	CONSTRAINT [PK_Attributes] PRIMARY KEY CLUSTERED ([AttributeId])
 );
+GO	
+
+INSERT INTO [dbo].[Attributes] ([SuperheroId],[PowerId]) VALUES (1,1)
 GO
+INSERT INTO [dbo].[Attributes] ([SuperheroId],[PowerId]) VALUES (1,4)
+GO
+
+SELECT Superhero.Name AS 'Superhero name', Power.Name AS 'Power name' FROM Attributes
+	INNER JOIN Superhero ON Superhero.SuperheroId = Attributes.SuperheroId
+	INNER JOIN Power ON Power.PowerId = Attributes.PowerId
+
+
+
 
